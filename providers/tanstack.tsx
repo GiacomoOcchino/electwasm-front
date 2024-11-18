0e8ab84649337892fc4ad4925d2fb0169bfd5f0a
GiacomoOcchino <giacomoocchino16@gmail.com>
2024-11-18 13:40:33 +0100

    feat: :heavy_plus_sign: Add tanstack query with Provider and QueryOption

---
+++ providers/tanstack.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { contractOption } from "@/queries/options";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient(contractOption);
+const TanstackProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
---
+++ queries/options.ts
@@ -0,0 +1,9 @@
+export const contractOption = {
+    defaultOptions:{
+        queries:{
+            gcTime: 1000*60*60*24, //24H
+            staleTime:1000*30, //30sec
+            refetchInterval:1000*30
+        }
+    }
+}
