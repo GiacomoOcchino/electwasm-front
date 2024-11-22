8ee941d116c70147a49f9ceff5facc90c870077c
GiacomoOcchino <giacomoocchino16@gmail.com>
2024-11-22 14:04:57 +0100

    feat: :label: Udpdate store

--- store/store.ts
+++ store/store.ts
@@ -1,16 +1,26 @@
 import { WalletStateType } from "@/types";
-import { Wallet } from "lucide-react";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+
+// Inizializza lo store
 const useStore = create<WalletStateType>()(
   persist(
     (set) => ({
       wallet: null,
       isAuthenticated: false,
       isLoading: true,
+      admin: null,
+      commissions: [],
+      voting_fee: 0,
       setWallet: (wallet) =>
         set({ wallet, isAuthenticated: !!wallet, isLoading: false }),
       setLoading: (loading) => set({ isLoading: loading }),
+      setContractState: (state) =>
+        set({
+          admin: state.admin,
+          commissions: state.commissions,
+          voting_fee: state.voting_fee,
+        }),
     }),
     {
       name: "wallet-storage",
