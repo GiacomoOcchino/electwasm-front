27655ffb9d25729f5e7ad6b72b21933bffea1063
GiacomoOcchino <giacomoocchino16@gmail.com>
2024-11-24 18:51:00 +0100

    feat: :sparkles: Add mutation for vote, add and remove

--- hooks/contract-mutation.tsx
+++ hooks/contract-mutation.tsx
@@ -118,3 +118,152 @@ export const useAskToJoinProposal = (senderAddress: string | undefined, proposal
     },
   });
 };
+export const useAddToProposal = (senderAddress: string | undefined, proposal_id:number) => {
+  return useMutation({
+    mutationFn: async ({ addresses }: { addresses: string[] }) => {
+        console.log('qui',senderAddress);
+        console.log('qui',proposal_id);
+  //  return;
+      const offlineSigner: OfflineSigner = window.getOfflineSigner!(CHAIN_ID);
+
+      const cosmwasmClient: SigningCosmWasmClient | null =
+        await SigningCosmWasmClient.connectWithSigner(
+          JUNO_TESTNET_RPC,
+          offlineSigner
+        );
+
+      if (!cosmwasmClient) throw new Error("Cosmwasm client is not connected");
+      // Accedi allo stato dello store
+      const { voting_fee } = useStore.getState();
+      // Trova la commissione necessaria per questa transazione (se applicabile)
+      const real_fee = {
+        amount: [
+          {
+            denom: "ujuno",
+            amount: "5000", // Gas fees separate from payment
+          },
+        ],
+        gas: "2000000",
+      };
+      const initMsg ={
+        add:addresses,
+        ask:"",
+        proposal_id:proposal_id*1,
+        rmv:[],
+      }
+      const msg = { update_voters: initMsg };
+      console.log(JSON.stringify(msg, null, 2));
+
+      if (senderAddress) {
+        const result = await cosmwasmClient.execute(
+          senderAddress,
+          CONTRACT_ADDRESS,
+          msg,
+          real_fee, // Puoi specificare un oggetto fee qui se necessario
+        );
+        console.table(result);
+        return result;
+      }
+      // Invia la transazione con le fee
+    },
+  });
+};
+export const useRemoveFromProposal = (senderAddress: string | undefined, proposal_id:number) => {
+  return useMutation({
+    mutationFn: async ({ addresses }: { addresses: string[] }) => {
+        console.log('qui',senderAddress);
+        console.log('qui',proposal_id);
+  //  return;
+      const offlineSigner: OfflineSigner = window.getOfflineSigner!(CHAIN_ID);
+
+      const cosmwasmClient: SigningCosmWasmClient | null =
+        await SigningCosmWasmClient.connectWithSigner(
+          JUNO_TESTNET_RPC,
+          offlineSigner
+        );
+
+      if (!cosmwasmClient) throw new Error("Cosmwasm client is not connected");
+      // Accedi allo stato dello store
+      const { voting_fee } = useStore.getState();
+      // Trova la commissione necessaria per questa transazione (se applicabile)
+      const real_fee = {
+        amount: [
+          {
+            denom: "ujuno",
+            amount: "5000", // Gas fees separate from payment
+          },
+        ],
+        gas: "2000000",
+      };
+      const initMsg ={
+        add:[],
+        ask:"",
+        proposal_id:proposal_id*1,
+        rmv:addresses,
+      }
+      const msg = { update_voters: initMsg };
+      console.log(JSON.stringify(msg, null, 2));
+
+      if (senderAddress) {
+        const result = await cosmwasmClient.execute(
+          senderAddress,
+          CONTRACT_ADDRESS,
+          msg,
+          real_fee, // Puoi specificare un oggetto fee qui se necessario
+        );
+        console.table(result);
+        return result;
+      }
+      // Invia la transazione con le fee
+    },
+  });
+};
+export const useVoteProposal = (senderAddress: string | undefined, proposal_id:number) => {
+  return useMutation({
+    mutationFn: async (vote: string) => {
+        console.log('qui',vote);
+        console.log('qui',proposal_id);
+        console.log('Number(vote)',Number(vote));
+  //  return;
+      const offlineSigner: OfflineSigner = window.getOfflineSigner!(CHAIN_ID);
+
+      const cosmwasmClient: SigningCosmWasmClient | null =
+        await SigningCosmWasmClient.connectWithSigner(
+          JUNO_TESTNET_RPC,
+          offlineSigner
+        );
+
+      if (!cosmwasmClient) throw new Error("Cosmwasm client is not connected");
+      // Accedi allo stato dello store
+      const { voting_fee } = useStore.getState();
+      // Trova la commissione necessaria per questa transazione (se applicabile)
+      const real_fee = {
+        amount: [
+          {
+            denom: "ujuno",
+            amount: voting_fee.toString(), // Gas fees separate from payment
+          },
+        ],
+        gas: "2000000",
+      };
+      const initMsg ={
+        vote:Number(vote),
+        proposal_id:proposal_id*1,
+      }
+      const msg = { vote: initMsg };
+      console.log(JSON.stringify(msg, null, 2));
+
+      if (senderAddress) {
+        const result = await cosmwasmClient.execute(
+          senderAddress,
+          CONTRACT_ADDRESS,
+          msg,
+          real_fee, 
+        );
+        // console.table(result);
+        return result;
+      }
+    },
+  });
+};
+
