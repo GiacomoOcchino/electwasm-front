51e50afc2726add7828c79aaf93c7da456e9c7d1
GiacomoOcchino <giacomoocchino16@gmail.com>
2024-11-25 19:11:46 +0100

    fix: update admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -34,29 +34,29 @@ export default function AdminPage() {
   if (queryError) return <div>Errore: {queryError.message}</div>;
   return (
     <MaxWidthWrapper>
-        <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
-        <ProposalForm />
+      <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+      <ProposalForm />
 
-        <h2 className="text-xl font-semibold mt-6 mb-2">Le tue proposte</h2>
-        {queryLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <MyProposalList
-            proposals={proposals?.proposals}
-            setSelectedProposal={setSelectedProposal}
-          />
-        )}
-        {selectedProposal && (
-          <div>
-            <h3 className="text-lg font-semibold">Proposta Selezionata:</h3>
+      <h2 className="text-xl font-semibold mt-6 mb-2">Le tue proposte</h2>
+      {queryLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <MyProposalList
+          proposals={proposals?.proposals}
+          setSelectedProposal={setSelectedProposal}
+        />
+      )}
+      {selectedProposal && (
+        <div className="my-4">
+          <h3 className="text-lg font-semibold">Proposta Selezionata:</h3>
+          <div className="flex flex-col  gap-4 md:flex-row">
             <p>ID: {selectedProposal[0]}</p>
             <p>Titolo: {selectedProposal[1]}</p>
-            {/* Esegui altre azioni */}
           </div>
-        )}
-        {selectedProposal && (
-          <VoterManagement proposalId={selectedProposal[0]} />
-        )}
+          {/* Esegui altre azioni */}
+        </div>
+      )}
+      {selectedProposal && <VoterManagement proposalId={selectedProposal[0]} />}
     </MaxWidthWrapper>
   );
 }
