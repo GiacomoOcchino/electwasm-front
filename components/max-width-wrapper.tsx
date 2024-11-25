67381a309c10b93a98829e15061dc076f8f6a4fa
GiacomoOcchino <giacomoocchino16@gmail.com>
2024-11-25 19:15:13 +0100

    style: :lipstick: update max-width-wrapper

--- components/max-width-wrapper.tsx
+++ components/max-width-wrapper.tsx
@@ -8,7 +8,7 @@ const MaxWidthWrapper = ({
     children: ReactNode
 }) => {
     return (
-        <section className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>
+        <section className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20 mb-4", className)}>
             {children}
         </section>
     )
