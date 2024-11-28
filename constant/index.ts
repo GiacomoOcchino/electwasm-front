bea5d32fb2858125d26bdff30f865f9f7ff464a2
GiacomoOcchino <giacomoocchino16@gmail.com>
2024-11-28 19:57:41 +0100

    fix: :bug: Update to new version Contract

--- constant/index.ts
+++ constant/index.ts
@@ -1,4 +1,4 @@
 export const CHAIN_ID = "uni-6";
 export const JUNO_TESTNET_RPC = 'https://juno-testnet-rpc.polkachu.com/';  // RPC
 export const JUNO_TESTNET_REST = 'https://juno-testnet-api.polkachu.com/'; // REST
-export const CONTRACT_ADDRESS = "juno130tncyw3daa86devguparlxc3dtp0g84ycelatl20hnu6zayy05spje00z"
+export const CONTRACT_ADDRESS = "juno1nl6h6yfkzzth2pvzq2jmlwu6q5zfg9mjd8lrf7a2zrh36d72dv6ssrdmer"
