7d6a9ee3a3e17517b9ba3b9e317dca2daf3e47a2
GiacomoOcchino <giacomoocchino16@gmail.com>
2024-11-29 21:13:04 +0100

    feat: :recycle: Update contract address

--- constant/index.ts
+++ constant/index.ts
@@ -1,4 +1,5 @@
 export const CHAIN_ID = "uni-6";
 export const JUNO_TESTNET_RPC = 'https://juno-testnet-rpc.polkachu.com/';  // RPC
 export const JUNO_TESTNET_REST = 'https://juno-testnet-api.polkachu.com/'; // REST
-export const CONTRACT_ADDRESS = "juno1nl6h6yfkzzth2pvzq2jmlwu6q5zfg9mjd8lrf7a2zrh36d72dv6ssrdmer"
+export const CONTRACT_ADDRESS = "juno1247684erfcu232su8uet2t46390jeh5r60h7x32kav7fnrg7q7gsvsm9tf"
+// export const CONTRACT_ADDRESS = "juno1nl6h6yfkzzth2pvzq2jmlwu6q5zfg9mjd8lrf7a2zrh36d72dv6ssrdmer"
